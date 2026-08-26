d9f18e82c9cd8fb44cdc2f87759d196934f19755
Tobias Junghans <tobydox@veyon.io>
2026-08-26 13:10:22 +0200

    PipeWireVncServer: drop unused client tracking mechanism
    (cherry picked from commit 8fe1b302b8b300ac83e3ae2ed5a83edb738b59aa)

--- plugins/vncserver/pipewire/PipeWireVncServer.h
+++ plugins/vncserver/pipewire/PipeWireVncServer.h
@@ -125,8 +125,6 @@ private Q_SLOTS:
 	void onPortalStarted();
 	void onPortalFailed();
 	void onStreamEnded();
-	void onFirstClientConnected();
-	void onLastClientDisconnected();
 
 private:
 	bool initVncServer(int serverPort, const Password& password);
@@ -138,10 +136,6 @@ private:
 	// LibVNCServer input event callbacks (C linkage function pointers)
 	static void onKbdAddEvent(rfbBool down, rfbKeySym keySym, rfbClientRec* cl);
 	static void onPtrAddEvent(int buttonMask, int x, int y, rfbClientRec* cl);
-
-	// LibVNCServer client lifecycle callbacks
-	static enum rfbNewClientAction onNewClientHook(rfbClientRec* cl);
-	static void onClientGone(rfbClientRec* cl);
 
 	PortalSession*       m_portalSession{nullptr};
 	PipeWireFramebuffer* m_framebuffer{nullptr};
